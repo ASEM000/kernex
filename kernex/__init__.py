b6a198584c751e9c90cdb0777ac68a3c24699208
Mahmoud Asem <48389287+ASEM000@users.noreply.github.com>
2022-07-12 01:26:20 +0900

    Update __init__.py

--- kernex/__init__.py
+++ kernex/__init__.py
@@ -4,4 +4,4 @@ from .src.base import kernelOperation
 from .src.map import baseKernelMap, kernelMap, offsetKernelMap
 from .src.scan import baseKernelScan, kernelScan, offsetKernelScan
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"

5d0b2b9e0dbd2c9733f3b68fb8852c446f4fda84
ASEM000 <mahmoudasem00@gmail.com>
2022-07-11 15:04:34 +0900

    add tests, fix __init__

--- kernex/__init__.py
+++ kernex/__init__.py
@@ -1,5 +1,5 @@
 from . import src
-from .interface.kernex_class import kscan, smap, sscan
+from .interface.kernex_class import kmap, kscan, smap, sscan
 from .src.base import kernelOperation
 from .src.map import baseKernelMap, kernelMap, offsetKernelMap
 from .src.scan import baseKernelScan, kernelScan, offsetKernelScan
